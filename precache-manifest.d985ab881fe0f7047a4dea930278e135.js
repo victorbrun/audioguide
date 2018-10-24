1c80547b7174ea1ece6cd4a3c947a5af3acd3d13
Victor Lebrun <victorl@theodo.fr>
2018-10-24 19:54:37 +0200

    Updates

---
+++ precache-manifest.d985ab881fe0f7047a4dea930278e135.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "79f6b6a0c4f3cca321dc458fe7e6544a",
+    "url": "/blindtest-spotify/static/media/logo.79f6b6a0.svg"
+  },
+  {
+    "revision": "9d4cbf40dcdac09bda6fa0aff397246b",
+    "url": "/blindtest-spotify/static/media/loading.9d4cbf40.svg"
+  },
+  {
+    "revision": "bd4de7ae32d74b318af5",
+    "url": "/blindtest-spotify/static/js/runtime~main.bd4de7ae.js"
+  },
+  {
+    "revision": "9fe4d90b92b4a4b58504",
+    "url": "/blindtest-spotify/static/js/main.9fe4d90b.chunk.js"
+  },
+  {
+    "revision": "3f7525847cbf7fd2fe0c",
+    "url": "/blindtest-spotify/static/js/1.3f752584.chunk.js"
+  },
+  {
+    "revision": "9fe4d90b92b4a4b58504",
+    "url": "/blindtest-spotify/static/css/main.0ac56afc.chunk.css"
+  },
+  {
+    "revision": "390cff253aecaffe2ba3730d496fe900",
+    "url": "/blindtest-spotify/index.html"
+  }
+];
